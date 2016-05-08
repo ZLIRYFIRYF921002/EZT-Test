2a500a4f177ac3d94262d5595fc1f3864518b593
fanpeng@eztcn.com <fanpeng@eztcn.com>
2016-05-08 03:43:34 -0700

    初始化工程

---
+++ 3333/3333/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3333
+//
+//  Created by tusm on 16/5/8.
+//  Copyright © 2016年 tusm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3333/3333/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3333
+//
+//  Created by tusm on 16/5/8.
+//  Copyright © 2016年 tusm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
